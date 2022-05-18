e3506eaec59b7ac31d0122e44a75c857708e4f5b
李志远 <785068529@qq.com>
2022-05-18 14:52:37 +0800

    init2

--- app/src/main/java/com/example/hub2/MainActivity.java
+++ app/src/main/java/com/example/hub2/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hub2");
+        System.out.println("hubg");
     }
 }
